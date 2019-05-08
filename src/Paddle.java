6bc642d119c0d6d64c5acd2f0b3320921545970e
cheeseer <43707262+cheeseer@users.noreply.github.com>
2019-05-08 03:15:13 -0700

    added an absolute path for the proper paddle image. The unfinished actmethod relies on code in the World class, but that's not written yet.

--- src/Paddle.java
+++ src/Paddle.java
@@ -1,14 +1,21 @@
 import javafx.scene.image.Image;
+import javafx.scene.input.KeyCode;
 
 public class Paddle extends Actor {
-
+	Image image = new Image("file:///C:/Users/pc/Desktop/ap cs game engine demo pics/paddle.png");
+	
     public Paddle() {
-        super();
-        super.setImage(new Image("https://i.imgur.com/ruMcsT2.jpg"));
+    	setImage(image); 
     }
 
     @Override
     public void act(long now) {
-        // do nothing
+    	if (getWorld().isKeyDown(KeyCode.LEFT)) {
+    		//add code to world first
+    		//add code to move paddle to the left
+    	}if (getWorld().isKeyDown(KeyCode.RIGHT)) {
+    		//add code to world first
+    		//add code to move paddle to the right
+    	}
     }
 }
